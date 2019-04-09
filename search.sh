a8d28ec4587e2f0815d1ae4ecc6754b81e81ed85
ostanioni <ostanioni@gmail.com>
2019-04-09 16:47:56 +0700

    grep command added

--- search.sh
+++ search.sh
@@ -5,3 +5,17 @@ $ updatedb
 $ locate *.h 
 # регистронезависимый поиск
 $ locate -i
+# grep позволяет организовать поиск информации в составе файлов
+$ grep any_text file.txt
+$ grep str *
+# recursive search --recursive
+$ grep -R
+# -i --ignore-case
+$ grep -i
+# -w --word-regexp
+$ grep -w
+# -n --line-number
+$ grep -n
+# examples
+$ ls -l | grep 1960
+$ ps -fU root | grep any_text
